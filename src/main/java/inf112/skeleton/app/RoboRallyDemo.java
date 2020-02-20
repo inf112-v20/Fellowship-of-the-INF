d4d35789d8bd6e3e6660ebd3e963cef183db6472
Henrik Bellsund <Henrik.Bellsund@student.uib.no>
2020-02-20 21:21:20 +0100

    made MainMenu class, not running yet

--- src/main/java/inf112/skeleton/app/RoboRallyDemo.java
+++ src/main/java/inf112/skeleton/app/RoboRallyDemo.java
@@ -1,16 +1,26 @@
 package inf112.skeleton.app;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import inf112.skeleton.app.Screen.MainMenuScreen;
 
 /**
  * Game to send to the application
  * Has gameScreen, which contains all the logic. This of course will be changed later.
  */
 public class RoboRallyDemo extends Game {
+
+    public SpriteBatch batch;
+
     @Override
     public void create() {
+        batch = new SpriteBatch();
+
         GameScreen gameScreen = new GameScreen();
-        setScreen(gameScreen);
+//        setScreen(gameScreen);
+        // TEST: Main menu screen
+        MainMenuScreen menuScreen = new MainMenuScreen();
+        setScreen(menuScreen);
     }
 
     @Override
---
+++ src/main/java/inf112/skeleton/app/Screen/MainMenuScreen.java
@@ -0,0 +1,68 @@
+package inf112.skeleton.app.Screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import inf112.skeleton.app.RoboRallyDemo;
+
+public class MainMenuScreen implements Screen {
+
+    private RoboRallyDemo game;
+
+    private Texture playButtonActive;
+    private Texture playButtonInactive;
+    private Texture exitButtonActive;
+    private Texture exitButtonInactive;
+
+    public MainMenuScreen (RoboRallyDemo game) {
+        this.game = game;
+
+        playButtonActive = new Texture("play_button_active.png");
+        playButtonInactive = new Texture("play_button_inactive.png");
+        exitButtonActive = new Texture("exit_button_active.png");
+        exitButtonInactive = new Texture("exit_button_inactive.png");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+
+        game.batch.draw(exitButtonActive, 100, 100);
+
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+}
