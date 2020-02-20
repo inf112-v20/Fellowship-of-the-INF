afc4a76294a0cca959ad68e9a7290353fd6a418b
JohannaJo <Lisa.josang@student.uib.no>
2020-02-20 20:11:34 +0100

    Added functionality to a lot of BoardPieces
    BoardPieceGenerator now generates more different types of pieces.

--- src/main/java/inf112/skeleton/app/GridObjects/BoardPieceGenerator.java
+++ src/main/java/inf112/skeleton/app/GridObjects/BoardPieceGenerator.java
@@ -19,8 +19,6 @@ public class BoardPieceGenerator {
      */
     public BoardPiece generate(int id) {
         switch (id) {
-            case (0):
-                return null;
             case (5):
                 return new FloorPiece(pos, id);
             case (15):
@@ -29,32 +27,102 @@ public class BoardPieceGenerator {
                 return new OpCardPiece(pos, id);
             case (6):
                 return new AbyssPiece(pos, id);
-            /**
-             * TODO: resolve problem of many different types of conveyor belts
-             * See conveyorbeltLayer
-             */
             case (49):
-                return new ConveyorBeltPiece(pos, id);
+                return new ConveyorBeltPiece(pos, id, Direction.NORTH, false, false);
+            case (50):
+                return new ConveyorBeltPiece(pos, id, Direction.SOUTH, false, false);
+            case (51):
+                return new ConveyorBeltPiece(pos, id, Direction.WEST, false, false);
+            case (52):
+                return new ConveyorBeltPiece(pos, id, Direction.EAST, false, false);
+            case (33):
+                return new ConveyorBeltPiece(pos, id, Direction.WEST, true, false);
+            case (34):
+                return new ConveyorBeltPiece(pos, id, Direction.NORTH, true, false);
+            case (35):
+                return new ConveyorBeltPiece(pos, id, Direction.NORTH, true, true);
+            case (36):
+                return new ConveyorBeltPiece(pos, id, Direction.EAST, true, true);
+            case (41):
+                return new ConveyorBeltPiece(pos, id, Direction.SOUTH, true, false);
+            case (42):
+                return new ConveyorBeltPiece(pos, id, Direction.EAST, true, false);
+            case (43):
+                return new ConveyorBeltPiece(pos, id, Direction.WEST, true, true);
+            case (44):
+                return new ConveyorBeltPiece(pos, id, Direction.SOUTH, true, true);
             case (13):
-                return new ExpressBeltPiece(pos, id);
-            /**
-             * TODO: resolve problem of having clockwise and counterclockwise cogs
-             */
+                return new ExpressBeltPiece(pos, id, Direction.NORTH, false, false);
+            case (14):
+                return new ExpressBeltPiece(pos, id, Direction.EAST, false, false);
+            case (21):
+                return new ExpressBeltPiece(pos, id, Direction.SOUTH, false, false);
+            case (22):
+                return new ExpressBeltPiece(pos, id, Direction.WEST, false, false);
+            case (25):
+                return new ExpressBeltPiece(pos, id, Direction.SOUTH, true, false);
+            case (26):
+                return new ExpressBeltPiece(pos, id, Direction.EAST, true, false);
+            case (27):
+                return new ExpressBeltPiece(pos, id, Direction.WEST, true, true);
+            case (28):
+                return new ExpressBeltPiece(pos, id, Direction.SOUTH, true, true);
+            case (17):
+                return new ExpressBeltPiece(pos, id, Direction.WEST, true, false);
+            case (18):
+                return new ExpressBeltPiece(pos, id, Direction.NORTH, true, false);
+            case (19):
+                return new ExpressBeltPiece(pos, id, Direction.NORTH, true, true);
+            case (20):
+                return new ExpressBeltPiece(pos, id, Direction.EAST, true, true);
             case (53):
+                return new CogPiece(pos, id, false);
             case (54):
-                return new CogPiece(pos, id);
-            /**
-             * TODO: find id of pusher
-             */
-            case (-1):
-                return new PusherPiece(pos, id);
-            /**
-             * TODO: resolve problem of having horizontal and vertical lazers
-             */
+                return new CogPiece(pos, id, true);
+            case (1):
+                return new PusherPiece(pos, id, Direction.SOUTH, false);
+            case (2):
+                return new PusherPiece(pos, id, Direction.WEST, false);
+            case (3):
+                return new PusherPiece(pos, id, Direction.NORTH, false);
+            case (4):
+                return new PusherPiece(pos, id, Direction.EAST, false);
+            case (9):
+                return new PusherPiece(pos, id, Direction.SOUTH, true);
+            case (10):
+                return new PusherPiece(pos, id, Direction.WEST, true);
+            case (11):
+                return new PusherPiece(pos, id, Direction.NORTH, true);
+            case (12):
+                return new PusherPiece(pos, id, Direction.EAST, true);
             case (47):
-                return new LazerPiece(pos, id);
+                return new LazerPiece(pos, id, Direction.NORTH, false, false);
+            case (39):
+                return new LazerPiece(pos, id, Direction.WEST, false, false);
+            case (40):
+                return new LazerPiece(pos, id, Direction.WEST, false, true);
+            case (103):
+                return new LazerPiece(pos, id, Direction.WEST, true, false);
+            case (102):
+                return new LazerPiece(pos, id, Direction.NORTH, true, false);
+            case (101):
+                return new LazerPiece(pos, id, Direction.NORTH, true, true);
             case (37):
-                return new LazerSourcePiece(pos, id);
+                return new LazerSourcePiece(pos, id, Direction.NORTH, false);
+            case (38):
+                return new LazerSourcePiece(pos, id, Direction.EAST, false);
+            case (45):
+                return new LazerSourcePiece(pos, id, Direction.SOUTH, false);
+            case (46):
+                return new LazerSourcePiece(pos, id, Direction.WEST, false);
+            case (88):
+                return new LazerSourcePiece(pos, id, Direction.NORTH, true);
+            case (93):
+                return new LazerSourcePiece(pos, id, Direction.EAST, true);
+            case (94):
+                return new LazerSourcePiece(pos, id, Direction.SOUTH, true);
+            case (95):
+                return new LazerSourcePiece(pos, id, Direction.WEST, true);
             case (31):
                 return new WallPiece(pos, id, Direction.NORTH);
             case (30):
@@ -64,10 +132,13 @@ public class BoardPieceGenerator {
             case (29):
                 return new WallPiece(pos, id, Direction.SOUTH);
             case (55):
+                return new FlagPiece(pos, id, 1);
             case (63):
+                return new FlagPiece(pos, id, 2);
             case (71):
+                return new FlagPiece(pos, id, 3);
             case (79):
-                return new FlagPiece(pos, id);
+                return new FlagPiece(pos, id, 4);
             //TODO: find id of player
             default:
                 return new NullPiece(pos, id);
--- src/main/java/inf112/skeleton/app/GridObjects/MovablePiece.java
+++
@@ -1,7 +0,0 @@
-package inf112.skeleton.app.GridObjects;
-
-public abstract class MovablePiece extends BoardPiece {
-    public MovablePiece(Position pos, int id) {
-        super(pos, id);
-    }
-}
