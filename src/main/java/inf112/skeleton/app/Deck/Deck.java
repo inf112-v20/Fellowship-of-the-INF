d91fca6278e6b28a2749714c54cf55455e154241
Lena Lussand Iversen <liv002@student.uib.no>
2020-02-24 17:31:20 +0100

    How many cards that are of each type is now decided by a final int

--- src/main/java/inf112/skeleton/app/Deck/Deck.java
+++ src/main/java/inf112/skeleton/app/Deck/Deck.java
@@ -10,9 +10,17 @@ import inf112.skeleton.app.Cards.ProgramCard;
 import java.util.ArrayList;
 import java.util.Collections;
 
-//TODO add final NumberOfCardsOfType...
 public class Deck {
     private ArrayList<ProgramCard> deck;
+
+    private final int nrOfMove1 = 18;
+    private final int nrOfMove2 = 12;
+    private final int nrOfMove3 = 6;
+    private final int nrOfBackup = 6;
+    private final int nrOfrotateRight = 18;
+    private final int nrOfrotateLeft = 18;
+    private final int nrOfUturn = 6;
+
 
     public Deck() {
         deck = new ArrayList<ProgramCard>();
@@ -67,37 +75,37 @@ public class Deck {
     }
 
     private void addMove1() {
-        for (int i=0; i<18; i++)
+        for (int i=0; i<nrOfMove1; i++)
             deck.add(new ProgramCard(1, CardType.MOVE1));
     }
 
     private void addMove2() {
-        for (int i=0; i<12; i++)
+        for (int i=0; i<nrOfMove2; i++)
             deck.add(new ProgramCard(1, CardType.MOVE2));
     }
 
     private void addMove3() {
-        for (int i=0; i<6; i++)
+        for (int i=0; i<nrOfMove3; i++)
             deck.add(new ProgramCard(1, CardType.MOVE3));
     }
 
     private void addBackup() {
-        for (int i=0; i<6; i++)
+        for (int i=0; i<nrOfBackup; i++)
             deck.add(new ProgramCard(1, CardType.BACKUP));
     }
 
     private void addRotateRight() {
-        for (int i = 0; i < 18; i++)
+        for (int i = 0; i < nrOfrotateRight; i++)
             deck.add(new ProgramCard(1, CardType.ROTATERIGHT));
     }
 
     private void addRotateLeft() {
-        for (int i = 0; i < 18; i++)
+        for (int i = 0; i < nrOfrotateLeft; i++)
             deck.add(new ProgramCard(1, CardType.ROTATELEFT));
     }
 
     private void addUturn() {
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < nrOfUturn; i++)
             deck.add(new ProgramCard(1, CardType.UTURN));
     }
 }
