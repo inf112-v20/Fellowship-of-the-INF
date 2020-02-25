7de9e59ac4a9c6a7cac038e53d24c3da47087f97
JohannaJo <Lisa.josang@student.uib.no>
2020-02-25 14:02:57 +0100

    Added canGo in WallPiece, not yet implemented

--- src/main/java/inf112/skeleton/app/GridObjects/WallPiece.java
+++ src/main/java/inf112/skeleton/app/GridObjects/WallPiece.java
@@ -7,4 +7,9 @@ public class WallPiece extends DirectionedPiece {
     public WallPiece(Position pos, int id, Direction dir) {
         super(pos, id, dir);
     }
+
+
+    public boolean canGo(Direction dir) {
+        return true;
+    }
 }
