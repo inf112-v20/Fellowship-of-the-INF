3cc358d1ce5d3be9eb95517253aeff90af4c22a3
Lena Lussand Iversen <liv002@student.uib.no>
2020-02-25 10:32:56 +0100

    Refined deck class, added comments and priority

--- src/main/java/inf112/skeleton/app/Deck/Deck.java
+++ src/main/java/inf112/skeleton/app/Deck/Deck.java
@@ -12,38 +12,54 @@ import java.util.Collections;
 
 public class Deck {
     private ArrayList<ProgramCard> deck;
+    private ArrayList<Integer> priorities;
 
     private final int nrOfMove1 = 18;
     private final int nrOfMove2 = 12;
     private final int nrOfMove3 = 6;
     private final int nrOfBackup = 6;
-    private final int nrOfrotateRight = 18;
-    private final int nrOfrotateLeft = 18;
+    private final int nrOfRotateRight = 18;
+    private final int nrOfRotateLeft = 18;
     private final int nrOfUturn = 6;
-
+    private int nrOfCards;
 
     public Deck() {
         deck = new ArrayList<ProgramCard>();
+        priorities = new ArrayList<Integer>();
+        nrOfCards = nrOfMove1+nrOfMove2+nrOfMove3+nrOfBackup+nrOfRotateRight+nrOfRotateLeft+nrOfUturn;
     }
 
-    //Draw card from the top of the deck. This DOES removes the card from the deck
+    /**
+     * Draw card from the top of the deck. This DOES removes the card from the deck
+     *
+     * @param index The index of where in the deck you want to draw the card from
+     * @return The chosen card
+     */
     public ProgramCard drawCard(int index) {
         ProgramCard card = deck.get(index);
         deck.remove(index);
         return card;
     }
 
-    //get size of deck
+    /**
+     * Get size of deck
+     */
     public int size(){
         return deck.size();
     }
 
-    //shuffle deck
+    /**
+     * Shuffle the deck
+     */
     public void shuffle() {
         Collections.shuffle(deck);
     }
 
-    //TODO Explain
+    /**
+     * Move all the cards from a different deck into this one. This removes the cards from the other deck
+     *
+     * @param otherDeck A different deck that you want to move all the cards from, to this deck
+     */
     public void moveAll(Deck otherDeck){
         for(int i=0; i<otherDeck.size(); i++)
             deck.add(otherDeck.drawCard(0));
@@ -57,13 +73,21 @@ public class Deck {
         deck.remove(index);
     }
 
-    //Used when looking at a card. This DOES NOT remove the card from the deck
+    /**
+     * Used when looking at a card. This DOES NOT remove the card from the deck
+     *
+     * @param index The index of where in the deck you want to see the card
+     * @return The card at the index
+     */
     public ProgramCard seeCard(int index){
         return deck.get(index);
     }
 
-    //Add the cards used in a programCard deck. Should only be used when making a new deck
+    /**
+     * Add the cards used in a programCard deck. Should only be used when making a new deck
+     */
     public void createDeck() {
+        createPriority();
         addMove1();
         addMove2();
         addMove3();
@@ -74,38 +98,61 @@ public class Deck {
         shuffle();
     }
 
+    /**
+     * Creates a list with unique priority values. The value is a number between 200 and 200+(20* number of cards)
+     */
+    private void createPriority() {
+        for (int i=0; i<nrOfCards; i++)
+            priorities.add(200+(i*20));
+        Collections.shuffle(priorities);
+    }
+
     private void addMove1() {
-        for (int i=0; i<nrOfMove1; i++)
-            deck.add(new ProgramCard(1, CardType.MOVE1));
+        for (int i=0; i<nrOfMove1; i++) {
+            deck.add(new ProgramCard(priorities.get(0), CardType.MOVE1));
+            priorities.remove(0);
+        }
     }
 
     private void addMove2() {
-        for (int i=0; i<nrOfMove2; i++)
-            deck.add(new ProgramCard(1, CardType.MOVE2));
+        for (int i=0; i<nrOfMove2; i++) {
+            deck.add(new ProgramCard(priorities.get(0), CardType.MOVE2));
+            priorities.remove(0);
+        }
     }
 
     private void addMove3() {
-        for (int i=0; i<nrOfMove3; i++)
-            deck.add(new ProgramCard(1, CardType.MOVE3));
+        for (int i=0; i<nrOfMove3; i++) {
+            deck.add(new ProgramCard(priorities.get(0), CardType.MOVE3));
+            priorities.remove(0);
+        }
     }
 
     private void addBackup() {
-        for (int i=0; i<nrOfBackup; i++)
-            deck.add(new ProgramCard(1, CardType.BACKUP));
+        for (int i=0; i<nrOfBackup; i++) {
+            deck.add(new ProgramCard(priorities.get(0), CardType.BACKUP));
+            priorities.remove(0);
+        }
     }
 
     private void addRotateRight() {
-        for (int i = 0; i < nrOfrotateRight; i++)
-            deck.add(new ProgramCard(1, CardType.ROTATERIGHT));
+        for (int i = 0; i < nrOfRotateRight; i++) {
+            deck.add(new ProgramCard(priorities.get(0), CardType.ROTATERIGHT));
+            priorities.remove(0);
+        }
     }
 
     private void addRotateLeft() {
-        for (int i = 0; i < nrOfrotateLeft; i++)
-            deck.add(new ProgramCard(1, CardType.ROTATELEFT));
+        for (int i = 0; i < nrOfRotateLeft; i++) {
+            deck.add(new ProgramCard(priorities.get(0), CardType.ROTATELEFT));
+            priorities.remove(0);
+        }
     }
 
     private void addUturn() {
-        for (int i = 0; i < nrOfUturn; i++)
-            deck.add(new ProgramCard(1, CardType.UTURN));
+        for (int i = 0; i < nrOfUturn; i++) {
+            deck.add(new ProgramCard(priorities.get(0), CardType.UTURN));
+            priorities.remove(0);
+        }
     }
 }
