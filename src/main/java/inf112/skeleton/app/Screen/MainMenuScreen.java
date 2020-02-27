6a5af2625e11147a03135f110d834e00f7682098
Henrik Bellsund <Henrik.Bellsund@student.uib.no>
2020-02-27 21:34:43 +0100

    Added functionality to main menu buttons

--- src/main/java/inf112/skeleton/app/Screen/MainMenuScreen.java
+++ src/main/java/inf112/skeleton/app/Screen/MainMenuScreen.java
@@ -17,25 +17,25 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import inf112.skeleton.app.GameScreen;
 import inf112.skeleton.app.RoboRallyDemo;
 
 public class MainMenuScreen implements Screen {
 
     private RoboRallyDemo game;
+    private float width;
+    private float height;
 
     private Stage stage;
-    private Skin skin; // appearance of buttons
-    private TextureAtlas atlas;
-    private Table table;
-    private TextButton buttonPlay, buttonExit;
-    private BitmapFont white, black;
-    private Label heading;
-
     private Image background;
+    private Image logo;
     private ImageButton playButton;
+    private ImageButton exitButton;
 
     public MainMenuScreen (RoboRallyDemo game) {
         this.game = game;
+        this.width = 1200; // width and height from Main.java
+        this.height = 1200;
     }
 
     @Override
@@ -44,35 +44,44 @@ public class MainMenuScreen implements Screen {
 
         // picture = all actors on stage
         Sprite picture = new Sprite(new Texture("menu_background.png"));
-        background = new Image((new SpriteDrawable(picture)));
-        background.setSize(3600, 3600);
+        background = new Image(new SpriteDrawable(picture));
+        background.setSize(width, height);
         background.setPosition(0, 0);
         stage.addActor(background);
 
-        picture = new Sprite(new Texture("play_button_active.png"));
+        picture = new Sprite(new Texture("RoboRally_logo.png"));
+        logo = new Image(new SpriteDrawable(picture));
+        logo.setSize(logo.getWidth()*1.5f, logo.getHeight()*2);
+        logo.setPosition((width-logo.getWidth())/2, height-475);
+        stage.addActor(logo);
+
+        // Play button
+        picture = new Sprite(new Texture("play_button_up.png"));
         playButton = new ImageButton(new SpriteDrawable(picture));
-        playButton.setPosition(50,50);
+        // TODO make variables for padding
+        playButton.setPosition(125,150);
         playButton.addListener(new ClickListener() {
             @Override
             public void clicked (InputEvent event, float x, float y) {
-                System.out.println("TEST: Play button is clicked");
+                game.setScreen(new GameScreen());
             }
         });
         stage.addActor(playButton);
 
+        // Exit button
+        picture = new Sprite(new Texture("exit_button_up.png"));
+        exitButton = new ImageButton(new SpriteDrawable(picture));
+        exitButton.setPosition((width-exitButton.getWidth())-125,150);
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked (InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+        stage.addActor(exitButton);
+
         Gdx.input.setInputProcessor(stage);
 
-        // TODO sjekk ut resten av tutorial
-//        table = new Table();
-//        table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-//
-//        buttonExit = new TextButton("EXIT", )
-//
-//        // TODO add Atlas
-////        atlas = new TextureAtlas();
-//
-//        white = new BitmapFont(Gdx.files.internal("font/white.fnt"), false);
-//        black = new BitmapFont(Gdx.files.internal("font/black.fnt"), false);
     }
 
     @Override
