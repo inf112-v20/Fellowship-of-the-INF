d3604d295aa491ed413c29ea971773314f16e200
Martin Kydland <martin.kydland@student.uib.no>
2020-02-27 15:03:28 +0100

    Made PlayerTests class, nothing functional yet

---
+++ src/test/java/inf112/skeleton/app/PlayerTests/PlayerTests.java
@@ -0,0 +1,17 @@
+package inf112.skeleton.app.PlayerTests;
+
+import com.badlogic.gdx.Game;
+import org.junit.Test;
+import inf112.skeleton.app.*;
+import inf112.skeleton.app.GridObjects.*;
+import inf112.skeleton.app.Grid.*;
+import inf112.skeleton.app.Deck.*;
+import inf112.skeleton.app.Cards.*;
+
+public class PlayerTests {
+    GameScreen gameScreen = new GameScreen();
+
+    @Test
+    public void getPlayerTest(){
+    }
+}
