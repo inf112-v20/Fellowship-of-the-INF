66e8368105387c22ec5377e6db292fbede16a6be
JohannaJo <Lisa.josang@student.uib.no>
2020-02-27 13:05:23 +0100

    Added getTurnRight/Left in Direction

--- src/main/java/inf112/skeleton/app/Grid/Direction.java
+++ src/main/java/inf112/skeleton/app/Grid/Direction.java
@@ -9,7 +9,27 @@ public enum Direction {
             case NORTH: return Direction.SOUTH;
             case EAST: return Direction.WEST;
             case WEST: return Direction.EAST;
-            default: return null; //TODO deal with this
+            default: return null; //no other possible directions
+        }
+    }
+
+    public Direction getRightTurnDirection() {
+        switch(this) {
+            case SOUTH: return Direction.WEST;
+            case NORTH: return Direction.EAST;
+            case EAST: return Direction.SOUTH;
+            case WEST: return Direction.NORTH;
+            default: return null;
+        }
+    }
+
+    public Direction getLeftTurnDirection() {
+        switch(this) {
+            case SOUTH: return Direction.EAST;
+            case NORTH: return Direction.WEST;
+            case EAST: return Direction.NORTH;
+            case WEST: return Direction.SOUTH;
+            default: return null;
         }
     }
 
