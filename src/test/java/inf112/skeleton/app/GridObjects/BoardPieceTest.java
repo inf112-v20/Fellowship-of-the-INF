58bc6551d74224debb501e6b961a084381990fd5
JohannaJo <Lisa.josang@student.uib.no>
2020-02-27 20:05:43 +0100

    Added a BoardPieceTest and equals method in Position

---
+++ src/test/java/inf112/skeleton/app/GridObjects/BoardPieceTest.java
@@ -0,0 +1,38 @@
+package inf112.skeleton.app.GridObjects;
+
+import inf112.skeleton.app.Grid.Position;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BoardPieceTest {
+    BoardPiece boardPiece;
+
+    @Before
+    public void generateBoardPiece() {
+        this.boardPiece = new FloorPiece(new Position(0, 0), 1);
+    }
+
+
+    @Test
+    public void getPos() {
+        for (int x = 1; x <= 20; x++) {
+            for (int y = 1; y <= 20; y++) {
+                boardPiece = new FloorPiece(new Position(x, y), 1);
+                assertEquals(boardPiece.getPos(), new Position(x, y));
+            }
+        }
+    }
+
+    @Test
+    public void setPos() {
+        for (int x = 1; x <= 20; x++) {
+            for (int y = 1; y <= 20; y++) {
+                assertNotEquals(boardPiece.getPos(), new Position(x, y));
+                boardPiece.setPos(new Position(x, y));
+                assertEquals(boardPiece.getPos(), new Position( x, y));
+            }
+        }
+    }
+}
