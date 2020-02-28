22242dff935f1c164ac21b8784dfdbbe9e62241c
JohannaJo <Lisa.josang@student.uib.no>
2020-02-28 12:30:04 +0100

    Finished BoardPieceGeneratorTest

--- src/test/java/inf112/skeleton/app/GridObjects/BoardPieceGeneratorTest.java
+++ src/test/java/inf112/skeleton/app/GridObjects/BoardPieceGeneratorTest.java
@@ -17,9 +17,49 @@ public class BoardPieceGeneratorTest {
     public void initialize() {
         generator = new BoardPieceGenerator(0, 0);
     }
+
     @Test
     public void generateFloorPieceIsFloorPiece() {
-        BoardPiece piece = generator.generate(1);
+        BoardPiece piece = generator.generate(5);
         assertEquals(piece.getClass(), FloorPiece.class);
+    }
+
+    @Test
+    public void generateAbyssPieceIsAbyssPiece() {
+        BoardPiece piece = generator.generate(6);
+        assertEquals(piece.getClass(), AbyssPiece.class);
+    }
+
+    /**
+     * There are many types of flag pieces, with different id's
+     */
+    @Test
+    public void generateFlagPieceIsFlagPiece() {
+        BoardPiece piece = generator.generate(55);
+        assertEquals(piece.getClass(), FlagPiece.class);
+        piece = generator.generate(63);
+        assertEquals(piece.getClass(), FlagPiece.class);
+        piece = generator.generate(71);
+        assertEquals(piece.getClass(), FlagPiece.class);
+        piece = generator.generate(79);
+        assertEquals(piece.getClass(), FlagPiece.class);
+    }
+
+    @Test
+    public void xPositionInGeneratedPieceIsCorrect() {
+        for (int xValue = 0; xValue < 12; xValue++) {
+            generator = new BoardPieceGenerator(xValue, 0);
+            BoardPiece piece = generator.generate(5);
+            assertEquals(piece.getPos().getX(), xValue);
+        }
+    }
+
+    @Test
+    public void yPositionInGeneratedPieceIsCorrect() {
+        for (int yValue = 0; yValue < 12; yValue++) {
+            generator = new BoardPieceGenerator(0, yValue);
+            BoardPiece piece = generator.generate(5);
+            assertEquals(piece.getPos().getY(), yValue);
+        }
     }
 }
