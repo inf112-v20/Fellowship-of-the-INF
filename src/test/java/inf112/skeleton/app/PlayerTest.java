a71f2da782fbf98a63199b567957f77d1f72a694
Henrik Bellsund <Henrik.Bellsund@student.uib.no>
2020-02-28 10:32:25 +0100

    added playerTest

---
+++ src/test/java/inf112/skeleton/app/PlayerTest.java
@@ -0,0 +1,21 @@
+package inf112.skeleton.app;
+
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    void playerIsDeadTest() {
+
+    }
+
+}
