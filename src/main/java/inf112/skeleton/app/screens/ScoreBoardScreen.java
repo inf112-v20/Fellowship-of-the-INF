52da890c29e741175cdb3f81285977953d4ac3e1
ErlendBerntsen <Erlend.Berntsen@student.uib.no>
2020-03-12 13:52:37 +0100

    Player pictures are now shown on the scoreboard

--- src/main/java/inf112/skeleton/app/screens/ScoreBoardScreen.java
+++ src/main/java/inf112/skeleton/app/screens/ScoreBoardScreen.java
@@ -7,9 +7,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import inf112.skeleton.app.Game;
@@ -22,6 +20,7 @@ public class ScoreBoardScreen {
     private Game game;
     private Label.LabelStyle evenRowStyle;
     private Label.LabelStyle oddRowStyle;
+    private Texture whiteTexture = new Texture(Gdx.files.internal("white.png"));
 
     public ScoreBoardScreen(Game game) {
         this.game = game;
@@ -32,25 +31,21 @@ public class ScoreBoardScreen {
         TextureRegionDrawable myTexRegionDrawable = new TextureRegionDrawable(textureRegion);
         Image image = new Image(myTexRegionDrawable);
         image.setPosition(3600, 0);
+        stage.addActor(image);
 
-        Texture rowTexture = new Texture((Gdx.files.internal("white.png")));
-        Sprite backgroundColor1 = new Sprite(rowTexture);
-
-        float alpha = 0.75f;
-        backgroundColor1.setColor(0, 0, 0, alpha);
+        Sprite backgroundColor1 = new Sprite(whiteTexture);
+        backgroundColor1.setColor(Color.BLACK);
         evenRowStyle = new Label.LabelStyle();
         evenRowStyle.font = new BitmapFont();
         evenRowStyle.background = new SpriteDrawable(backgroundColor1);
         evenRowStyle.fontColor = Color.WHITE;
 
-        Sprite backgroundColor2 = new Sprite(rowTexture);
+        Sprite backgroundColor2 = new Sprite(whiteTexture);
         backgroundColor2.setColor(Color.DARK_GRAY);
         oddRowStyle = new Label.LabelStyle();
         oddRowStyle.font = new BitmapFont();
         oddRowStyle.background = new SpriteDrawable(backgroundColor2);
         oddRowStyle.fontColor = Color.WHITE;
-
-        stage.addActor(image);
         update();
     }
 
@@ -63,36 +58,52 @@ public class ScoreBoardScreen {
         Label titleLabel = new Label("SCOREBOARD", evenRowStyle);
         titleLabel.setFontScale(25);
         titleLabel.setAlignment(1);
-        table.add(titleLabel).fillX();
 
-        /*
+        TextureRegion whiteTR = new TextureRegion(whiteTexture);
+        TextureRegionDrawable whiteTRD =  new TextureRegionDrawable(whiteTR);
+        Image titleFiller = new Image(whiteTRD);
+        titleFiller.setColor(Color.BLACK);
+
+        table.add(titleFiller).fill();
+        table.add(titleLabel).fill();
         table.row();
-        Label testLabel1 = new Label("HELLO", oddRowStyle);
-        testLabel1.setFontScale(10);
-        Label testLabel2 = new Label("WORLD", oddRowStyle);
-        testLabel2.setFontScale(10);
-        table.add(testLabel1, testLabel2);
 
-         */
 
-        table.row();
-        String headLabelText  = "Name           Lifes           Damage          Flags";
+        String headLabelText  = "Name       Lifes       Damage      Flags";
         Label headLabel = new Label(headLabelText, oddRowStyle);
         headLabel.setFontScale(10);
-        table.add(headLabel).fillX().left();
+        Image headFiller = new Image(whiteTRD);
+        headFiller.setColor(Color.DARK_GRAY);
+        table.add(headFiller).fill();
+        table.add(headLabel).left().fill();
         table.row();
+
         for (int i = 0; i < game.getListOfPlayers().length ; i++) {
             Player player = game.getListOfPlayers()[i];
-            String text = "Player " + player.getPlayerNumber() + "          " + player.getLives() + "                   "
-                    + player.getDamage() + "                     " + player.getCheckpointsVisited();
+            String text = "Player " + player.getPlayerNumber() + "      " + player.getLives() + "               "
+                    + player.getDamage() + "                " + player.getCheckpointsVisited();
             Label playerLabel;
-            if(i%2 == 0){ playerLabel = new Label(text, evenRowStyle); }
-            else{ playerLabel = new Label(text, oddRowStyle); }
+            Image backgroundColor = new Image(whiteTRD);
+            if(i%2 == 0){
+                playerLabel = new Label(text, evenRowStyle);
+                backgroundColor.setColor(Color.BLACK);
+            }
+            else{
+                playerLabel = new Label(text, oddRowStyle);
+                backgroundColor.setColor(Color.DARK_GRAY);
+            }
             playerLabel.setFontScale(10);
-            table.add(playerLabel).fillX().left();
+            TextureRegion playerPicture = player.getPlayerCell().getTile().getTextureRegion();
+            TextureRegionDrawable playerPictureTRD = new TextureRegionDrawable(playerPicture);
+            Stack stack = new Stack();
+            stack.add(backgroundColor);
+            Image playerImage = new Image(playerPictureTRD);
+            stack.add(playerImage);
+            table.add(stack).fill();
+            table.add(playerLabel).expandX().left().fill();
             table.row();
         }
-        // table.debug();
+        //table.debug();
         stage.addActor(table);
     }
 
