aa3e98547cf4f1503094ef2a1d50b8433d4230dc
Erlend <Erlend.Berntsen@student.uib.no>
2020-03-30 13:31:51 +0200

    Created AIPlayer class that extends player. Contains all methods regarding choosing the best cards at round start.

---
+++ src/main/java/inf112/skeleton/app/RoboRallyGame.java
@@ -0,0 +1,21 @@
+package inf112.skeleton.app;
+
+import com.badlogic.gdx.Game;
+import inf112.skeleton.app.screens.MainMenuScreen;
+
+/**
+ * Game to send to the application
+ * Has gameScreen, which contains all the logic. This of course will be changed later.
+ */
+public class RoboRallyGame extends Game {
+    @Override
+    public void create() {
+        MainMenuScreen menuScreen = new MainMenuScreen(this);
+        setScreen(menuScreen);
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+}
