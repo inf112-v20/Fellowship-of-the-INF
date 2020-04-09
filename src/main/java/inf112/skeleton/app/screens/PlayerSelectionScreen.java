c94b75577e495e58726822e5800a04e5d060f558
JohannaJo <Lisa.josang@student.uib.no>
2020-04-09 12:01:03 +0200

    Fixed drop down menu for difficulty selection
    Note: the buttons don't do anything, only print out the difficulty you selected.

--- src/main/java/inf112/skeleton/app/screens/PlayerSelectionScreen.java
+++ src/main/java/inf112/skeleton/app/screens/PlayerSelectionScreen.java
@@ -6,9 +6,11 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import inf112.skeleton.app.RoboRallyGame;
@@ -109,19 +111,25 @@ public class PlayerSelectionScreen implements Screen {
         });
         stage.addActor(backButton);
 
-/*
-        Skin skin=new Skin( Gdx.files.internal("menu/navbuttons/uiskin.json"));
-        Dialog dialog=new Dialog("Difficulty",skin);
-        dialog.setSize(100,100);
-        dialog.setPosition(player1Button.getX(), player1Button.getY()*0.5f);
+        //Sin Source: https://github.com/libgdx/libgdx-skins/tree/master/skins/visui/assets
+        TextureAtlas dropdownAtlas = new TextureAtlas(Gdx.files.internal("menu/navbuttons/uiskin.atlas"));
+        Skin skin = new Skin(Gdx.files.internal("menu/navbuttons/uiskin.json"), dropdownAtlas);
+        Dialog dialog = new Dialog("Difficulty", skin);
+        dialog.setSize(100, 50);
+        dialog.setPosition(player1Button.getX(), player1Button.getY() * 0.5f);
         final SelectBox<String> dropDownMenu = new SelectBox<>(skin);
+        dropDownMenu.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                System.out.println(dropDownMenu.getSelected());
+            }
+        });
         dropDownMenu.setItems("EASY", "MEDIUM", "HARD", "EXPERT");
         dialog.getContentTable().defaults().pad(10);
         dialog.getContentTable().add(dropDownMenu);
         stage.addActor(dialog);
 
- */
-    Gdx.input.setInputProcessor(stage);
+        Gdx.input.setInputProcessor(stage);
 
     }
 
@@ -129,6 +137,7 @@ public class PlayerSelectionScreen implements Screen {
     public void render(float v) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f)); //added to fix dropDownMenu
         stage.draw();
     }
 
