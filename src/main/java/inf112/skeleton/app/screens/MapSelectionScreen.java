f419343971fb479b2e99c4ae7c2c1e12d0fe086b
Martin <Kydland@hotmail.com>
2020-05-05 13:59:24 +0200

    Implemented method for creating map buttons

--- src/main/java/inf112/skeleton/app/screens/MapSelectionScreen.java
+++ src/main/java/inf112/skeleton/app/screens/MapSelectionScreen.java
@@ -18,6 +18,9 @@ public class MapSelectionScreen implements Screen {
     private RoboRallyGame game;
     private float width;
     private float height;
+    private int xPadding;
+    private int yPadding;
+    private float MAP_SCALE = 0.25f;
 
     private int playerCount;
     private Difficulty difficulty;
@@ -33,7 +36,6 @@ public class MapSelectionScreen implements Screen {
 
     @Override
     public void show() {
-        float mapScale = 0.25f;
         stage = new Stage();
 
         // picture = all actors on stage
@@ -45,7 +47,7 @@ public class MapSelectionScreen implements Screen {
         stage.addActor(background);
 
         // Logo
-        int yPadding = 50;
+        yPadding = 50;
         picture = new Sprite(new Texture("menu/RoboRally_logo.png"));
         Image logo = new Image(new SpriteDrawable(picture));
         logo.setSize(logo.getWidth() * 1.5f, logo.getHeight() * 2);
@@ -58,102 +60,28 @@ public class MapSelectionScreen implements Screen {
         chooseMap.setPosition((width - chooseMap.getWidth()) / 2, logo.getY()-yPadding-chooseMap.getHeight()/2);
         stage.addActor(chooseMap);
 
-        int xPadding = 100;
-
-        ImageButton stage1Button;
-        ImageButton stage2Button;
-        ImageButton stage3Button;
-        ImageButton stage4Button;
-        ImageButton stage5Button;
-        ImageButton stage6Button;
-
-        //Stage 1 button
+        xPadding = 100;
         yPadding = 400;
-        picture = new Sprite(new Texture("menu/mapButtons/Spin.png"));
-        stage1Button = new ImageButton(new SpriteDrawable(picture));
-        stage1Button.setTransform(true);
-        stage1Button.setScale(mapScale);
+
+        ImageButton stage1Button = createMapButton("Spin");
         stage1Button.setPosition(xPadding, yPadding);
-        stage1Button.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                //Original map: RoborallyBoard_debugged.tmx
-                //Test map for conveyorbelts: conveyorBeltTestMap.tmx
-                game.setScreen(new GameScreen(game, "maps/RoborallyBoard_Spin.tmx", playerCount, difficulty));
-            }
-        });
-        stage.addActor(stage1Button);
-        // Stage 2 button
-        picture = new Sprite(new Texture("menu/mapButtons/Vault_Assault.png"));
-        stage2Button = new ImageButton(new SpriteDrawable(picture));
-        stage2Button.setPosition(stage1Button.getX() + xPadding + stage2Button.getWidth() * mapScale, yPadding);
-        stage2Button.setTransform(true);
-        stage2Button.setScale(mapScale);
-        stage2Button.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new GameScreen(game, "maps/RoborallyBoard_Vault_Assault.tmx", playerCount, difficulty));
-            }
-        });
-        stage.addActor(stage2Button);
 
-        // Stage 3 button
-        picture = new Sprite(new Texture("menu/mapButtons/Exchange.png"));
-        stage3Button = new ImageButton(new SpriteDrawable(picture));
-        stage3Button.setPosition(stage2Button.getX() + stage3Button.getWidth() * mapScale, yPadding);
-        stage3Button.setTransform(true);
-        stage3Button.setScale(mapScale);
-        stage3Button.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new GameScreen(game, "maps/RoborallyBoard_Exchange.tmx", playerCount, difficulty));
-            }
-        });
-        stage.addActor(stage3Button);
+        ImageButton stage2Button = createMapButton("Vault_Assault");
+        stage2Button.setPosition(stage1Button.getX() + xPadding + stage2Button.getWidth() * MAP_SCALE, yPadding);
 
-        // Stage 4 button
-        picture = new Sprite(new Texture("menu/mapButtons/Debugged.png"));
-        stage4Button = new ImageButton(new SpriteDrawable(picture));
-        stage4Button.setPosition(stage3Button.getX() + stage4Button.getWidth() * mapScale + xPadding / 2, yPadding);
-        stage4Button.setTransform(true);
-        stage4Button.setScale(mapScale);
-        stage4Button.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new GameScreen(game, "maps/RoborallyBoard_Debugged.tmx", playerCount, difficulty));
-            }
-        });
-        stage.addActor(stage4Button);
+        ImageButton stage3Button = createMapButton("Exchange");
+        stage3Button.setPosition(stage2Button.getX() + stage3Button.getWidth() * MAP_SCALE, yPadding);
 
-        yPadding -= stage1Button.getHeight() * mapScale + 50;
+        ImageButton stage4Button = createMapButton("Debugged");
+        stage4Button.setPosition(stage3Button.getX() + stage4Button.getWidth() * MAP_SCALE + xPadding / 2, yPadding);
 
-        // Stage 5 button
-        picture = new Sprite(new Texture("menu/mapButtons/Maelstrom.png"));
-        stage5Button = new ImageButton(new SpriteDrawable(picture));
+        yPadding -= stage1Button.getHeight() * MAP_SCALE + 50;
+
+        ImageButton stage5Button = createMapButton("Maelstrom");
         stage5Button.setPosition(stage1Button.getX(), yPadding);
-        stage5Button.setTransform(true);
-        stage5Button.setScale(mapScale);
-        stage5Button.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new GameScreen(game, "maps/RoborallyBoard_Maelstrom.tmx", playerCount, difficulty));
-            }
-        });
-        stage.addActor(stage5Button);
 
-        // Stage 6 button
-        picture = new Sprite(new Texture("menu/mapButtons/Vault.png"));
-        stage6Button = new ImageButton(new SpriteDrawable(picture));
-        stage6Button.setPosition(stage5Button.getX() + stage6Button.getWidth() * mapScale + xPadding, yPadding);
-        stage6Button.setTransform(true);
-        stage6Button.setScale(mapScale);
-        stage6Button.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new GameScreen(game, "maps/RoborallyBoard_Vault.tmx", playerCount, difficulty));
-            }
-        });
-        stage.addActor(stage6Button);
+        ImageButton stage6Button = createMapButton("Vault");
+        stage6Button.setPosition(stage5Button.getX() + stage6Button.getWidth() * MAP_SCALE + xPadding, yPadding);
 
         // Back button
         picture = new Sprite(new Texture("menu/navbuttons/BackButton.png"));
@@ -168,6 +96,22 @@ public class MapSelectionScreen implements Screen {
         stage.addActor(backButton);
 
         Gdx.input.setInputProcessor(stage);
+    }
+
+
+    public ImageButton createMapButton(final String mapName){
+        Sprite picture = new Sprite(new Texture("menu/mapButtons/" + mapName + ".png"));
+        ImageButton stageButton = new ImageButton(new SpriteDrawable(picture));
+        stageButton.setTransform(true);
+        stageButton.setScale(MAP_SCALE);
+        stageButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new GameScreen(game, "maps/RoborallyBoard_"+ mapName + ".tmx", playerCount, difficulty));
+            }
+        });
+        stage.addActor(stageButton);
+        return stageButton;
     }
 
     @Override
